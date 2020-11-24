acca772bba0a872aeb2e8121828f67f08b6ad940
ttamsmas <ttamsmas@gmail.com>
2020-11-24 12:20:48 -0800

    clearing readme

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
-  development: 'http://localhost:4741'
+  production: 'https://sei-library-api.herokuapp.com/books',
+  development: 'https://sei-library-api.herokuapp.com/books'
 }
 
 if (window.location.hostname === 'localhost') {
